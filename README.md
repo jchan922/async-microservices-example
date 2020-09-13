7583421cc640ee2d83e9554ec5c8d93b963ef196
Justin Chan <justinchan@Justins-MacBook-Pro.local>
2020-09-13 12:27:58 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Async Microservices Example
+
+## Posts and Comments App
+
+### Client uses:
+- create-react-app
+
+### Services use:
+- express
+- cors
+- axios
+- nodemon
---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PostCreate from './PostCreate';
+import PostList from './PostList';
+
+export default () => {
+    return (
+        <div className="container">
+            <h1>Blog</h1>
+
+            <h2>Create Post</h2>
+            <PostCreate />
+
+            <hr />
+
+            <h2>Posts</h2>
+            <PostList />
+        </div>
+    );
+};
