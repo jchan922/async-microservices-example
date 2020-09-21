d5c88d53e69a580255b3c44d985e63b084bd9a85
Justin Chan <jchan922@gmail.com>
2020-09-20 21:09:19 -0700

    connect remaining services through Kubernetes ClusterIPs

--- event-bus/index.js
+++ event-bus/index.js
@@ -13,9 +13,9 @@ app.post('/events', async (req, res) => {
     events.push(event);
 
     await axios.post('http://posts-service-clusterip-srv:4000/events', event);
-    // axios.post('http://localhost:4001/events', event);
-    // axios.post('http://localhost:4002/events', event);
-    // axios.post('http://localhost:4003/events', event);
+    await axios.post('http://comments-service-srv:4001/events', event);
+    await axios.post('http://query-service-srv:4002/events', event);
+    await axios.post('http://moderation-service-srv:4003/events', event);
 
     res.send({ status: 'OK' });
 });
